ba99920bfd894ab490b7e828661ea5fd0dcf4bfc
Philopater <philoodolf@gmail.com>
2024-02-24 12:44:52 +0200

    spalshscreen

--- app/src/main/java/com/example/work_flowapplication/MainActivity.kt
+++ app/src/main/java/com/example/work_flowapplication/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.work_flowapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.work_flowapplication.ui.theme.WorkFlowApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +28,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             WorkFlowApplicationTheme {
                 // A surface container using the 'background' color from the theme
+                splashscreen()
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                   
                 }
             }
         }
@@ -30,17 +41,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun splashscreen() {
+    ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+    val (logo)=createRefs()
+        Image(painter = painterResource(id =R.drawable.logo ), contentDescription ="logo" ,
+            modifier = Modifier.constrainAs(logo) {
+                top.linkTo(parent.top)
+                bottom.linkTo(parent.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            }.clip(RoundedCornerShape(size = 50.dp))
+
+        )
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     WorkFlowApplicationTheme {
-        Greeting("Android")
+        splashscreen()
     }
 }
